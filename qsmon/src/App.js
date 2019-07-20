4a87d25a223b2187e1f977eed5df43f4a79a693e
Clayton <sibandaclayton0@gmail.com>
2019-07-20 14:31:45 +0200

    react router now works

--- qsmon/src/App.js
+++ qsmon/src/App.js
@@ -5,22 +5,21 @@ import 'popper.js'
 import React from 'react';
 import './App.css';
 import CustomLayout from './containers/Layout';
-import Measurements from './components/measurements/measurements_home';
-import {BrowserRouter, Route } from 'react-router-dom';
-import Header from './components/header';
-import Results from './components/results/results_home';
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import CustomLayout from './containers/Layout'
 import CustomLine from './components/charts/Line';
+import Measurements from './components/measurements/measurements_home';
+import PingForm from './components/measurements/ping';
+import {BrowserRouter, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-     <CustomLayout>
-<CustomLine/>
-     </CustomLayout>
+  
+
+  <BrowserRouter>
+<CustomLayout ><Measurements/>
+</CustomLayout>
+
+</BrowserRouter>
     </div>
   );
 }
