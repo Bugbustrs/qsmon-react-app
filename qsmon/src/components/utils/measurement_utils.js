b70e1d939c426e38198e3a5b965978073a5ecf4d
Clayton <sibandaclayton0@gmail.com>
2019-07-23 12:11:21 +0200

    made all input forms controlled forms

--- qsmon/src/components/utils/measurement_utils.js
+++ qsmon/src/components/utils/measurement_utils.js
@@ -1,51 +1,18 @@
+import measurementList from "../measurements/measurement_list";
 
 function getKey()
 {
   return Math.random().toString(36).substring(2, 16) + Math.random().toString(36).substring(2, 16);
 }
 
-
-const formatDate = date =>{
-  return new Date(date).toISOString();
-}
 /*
 generates a post request object from the state of the app
 */
 function getPostObject(state)
 {
-
-let measurement_description = { 
-            type:state.type,
-            key:getKey(),
-            start_time:formatDate(state.start_time),
-            end_time:formatDate(state.end_time),
-            interval_sec:state.interval_sec,
-            priority:state.priority,
-          parameters:{
-           target:state.target,
-           server:"null",
-           direction:state.tcp_speed_test,   
-          }
-          };
-let job_description = {
-  measurement_description,
-  node_count:state.node_count,
-  job_interval:state.job_interval
-}
-
-let result={job_description, request_type:"SCHEDULE_MEASUREMENT" ,user_id:getKey(), };
-
-return result;
-}
-
-let format =val =>{
-  return val>9?val:'0'+val;
+    let result={};
+let measurement_description={
+    
 };
-
-const getType = type  =>{
-const map ={"/tcp_speed_test":"TCP Speed Test",'/ping':"Ping Test","/dns_lookup":"DNS Lookup Test",'/traceroute':'Traceroute Test','/http':'HTTP Test'};
-
-return map[type];
 }
-
-export  {format,getKey,getPostObject,getType}
+export  {getKey,getPostObject}
