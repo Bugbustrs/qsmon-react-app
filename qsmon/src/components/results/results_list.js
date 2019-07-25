57b8ebf8ea10ff3a3218293c051d8e04b1682413
Clayton <sibandaclayton0@gmail.com>
2019-07-25 12:03:43 +0200

    added new components for graph visualisation

--- qsmon/src/components/results/results_list.js
+++ qsmon/src/components/results/results_list.js
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function resultList ()
 {
     return(
-<ul className="list-group">
-  <li className="list-group-item">Cras justo odio</li>
-  <li className="list-group-item">Dapibus ac facilisis in</li>
-  <li className="list-group-item">Morbi leo risus</li>
-  <li className="list-group-item">Porta ac consectetur ac</li>
-  <li className="list-group-item">Vestibulum at eros</li>
+<ul className="list-group ">
+  <li className="list-group-item "><Link to='/results/ping'>Ping Results</Link></li>
+  <li className="list-group-item"><Link to='/results/traceroute'>Traceroute Results</Link></li>
+  <li className="list-group-item"><Link to='/results/dns'>DNS Results</Link></li>
+  <li className="list-group-item"><Link to='/results/http'>HTTP Results</Link></li>
+  <li className="list-group-item"><Link to='/results/tcp'>TCP Results</Link></li>
 </ul>
     );
 }
