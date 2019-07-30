4b95f3054a9d7363bb38a1100df6fe9b3b46d908
Clayton <sibandaclayton0@gmail.com>
2019-07-30 20:03:03 +0200

    update paths for the build folder

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+  // Priority serve any static files.
+app.use(express.static(path.join(__dirname, '../qsmon/build')));
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  // All remaining requests return the React app, so it can handle routing.
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../qsmon/build', 'index.html'));
 });
 app.listen(port);
