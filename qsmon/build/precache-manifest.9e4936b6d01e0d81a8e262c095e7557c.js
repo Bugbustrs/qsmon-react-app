20fa68c0f675de5de61a0a2a806126ee9ac967b2
Clayton <sibandaclayton0@gmail.com>
2019-07-30 15:45:40 +0200

    added build folder

---
+++ qsmon/build/precache-manifest.9e4936b6d01e0d81a8e262c095e7557c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d35cffc4bd6078bc79b451eebb8a42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4c6f0ba59c156d66364",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "27e9ae842d7d7f093fc5",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c4c6f0ba59c156d66364",
+    "url": "/static/js/2.7a1ab221.chunk.js"
+  },
+  {
+    "revision": "27e9ae842d7d7f093fc5",
+    "url": "/static/js/main.205f1dfd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
