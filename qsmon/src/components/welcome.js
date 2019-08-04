5ee6e59a861c73dfe34b2d3d5a916596c3e5ab8e
Clayton <sibandaclayton0@gmail.com>
2019-08-04 10:09:18 +0200

    finished the landing page

--- qsmon/src/components/welcome.js
+++ qsmon/src/components/welcome.js
@@ -5,9 +5,14 @@ export default function welcome(props)
     return (
         <div className='tes'>
             <div className='bg'>
+           <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCQKzqTqwQhE9ecrMnfRT7vm2x7UgKW9yilGwonbWAV6IXytL5xg'/>
+                <div className='banner mt-5 container'>
+                    <div className='jumbotron'>
+                    <h3 className='text-center'>Quality Of Service Monitoring Platform</h3>
+                    <h4>Honors project by:</h4> Clayton Sibanda, David Kheri and Meluleki Dube
+                    <h4>Supervised by:</h4> Dr Josiah Chavula
 
-                <div className='jumbotron jumbotron-fluid'>
-                    <h1 className='text-center'>Quality Of Service Monitoring Platform</h1>
+                    </div>
                 </div>
             </div>
         </div>
